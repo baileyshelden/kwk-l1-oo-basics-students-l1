648f87d4ddd9f5010ee4db2f9507b2f6e8c5c936
baileyshelden <bailey7748@gmail.com>
2018-06-07 22:27:01 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,41 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+  
+  def initialize(title, author, page_count, genre)
+    @title = title 
+  end 
+  
+  def title 
+  @title 
+  end 
+  
+  def author=(author)
+    @author = author 
+  end 
+  
+  def author
+  @author
+  end 
+  
+  def page_count=(num)
+    @page_count = num
+  end 
+  
+  def page_count 
+  @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre 
+  @genre
+  end 
+
+end 
+
+book_1 = Book.new("The Last Olympian", "Rick Riordan", "392", "Fantasy")
+book_2 = Book.new("Manners", "Moe Williams", "8", "Children's")
+book_3 = Book.new("Pretties", "Scott Westerfield", "392", "Dystopian")
+
+print book_1.title
