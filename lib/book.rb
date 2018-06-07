5028754751a9611abe55a8a307be26e572654132
baileyshelden <bailey7748@gmail.com>
2018-06-07 22:25:02 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -42,4 +42,4 @@ book_1 = Book.new("The Last Olympian", "Rick Riordan", "392", "Fantasy")
 book_2 = Book.new("Manners", "Moe Williams", "8", "Children's")
 book_3 = Book.new("Pretties", "Scott Westerfield", "392", "Dystopian")
 
-print book_1
+print book_1.title
