37462eb45004d634e10bcf0323fd4f3483abb4ab
baileyshelden <bailey7748@gmail.com>
2018-06-07 22:21:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,4 @@
-class book (title, author)
+class book (title, author, page_count, genre)
   
   def initialize(title)
     @title = title 
@@ -8,5 +8,38 @@ class book (title, author)
   @title 
   end 
   
+  def author = (author)
+    @author = author 
+  end 
+  
+  def author
+  @author
+  end 
+  
+  def page_count = (num)
+    @page_count = num
+  end 
+  
+  def page_count 
+  @page_count
+  end 
+  
+  def genre = (genre)
+    @genre = genre
+  end 
+  
+  def genre 
+  @genre
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
 end 
+
+book_1 = book.new("The Last Olympian", "Rick Riordan", "392", "Fantasy")
+book_2 = book.new("Manners", "Moe Williams", "8", "Children's")
+book_3 = book.new("Pretties", "Scott Westerfield", "392", "Dystopian")
+
 
